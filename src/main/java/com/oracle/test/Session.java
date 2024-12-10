218f7fb26deb6e42d295b8bd1dd3ff7f82119458
loic_lefevre <loic.lefevre@oracle.com>
2024-12-10 08:14:34 +0100

    Create schema service

--- src/main/java/com/oracle/test/Session.java
+++ src/main/java/com/oracle/test/Session.java
@@ -17,6 +17,7 @@ import java.net.http.HttpResponse;
 import java.util.Base64;
 
 import static com.oracle.test.Main.VERSION;
+import static com.oracle.test.exception.TestException.*;
 
 /**
  * Test session.
@@ -75,7 +76,7 @@ public class Session {
 						databaseType = DatabaseType.valueOf(args[++i]);
 					}
 					catch (IllegalArgumentException iae) {
-						throw new TestException(TestException.WRONG_DATABASE_TYPE_PARAMETER,
+						throw new TestException(WRONG_DATABASE_TYPE_PARAMETER,
 								new IllegalArgumentException("--db-type must be either atps, db19c, db21c, or db23ai"));
 					}
 					break;
@@ -86,7 +87,7 @@ public class Session {
 
 				default:
 					displayUsage();
-					throw new TestException(TestException.UNKNOWN_COMMAND_LINE_ARGUMENT);
+					throw new TestException(UNKNOWN_COMMAND_LINE_ARGUMENT);
 			}
 		}
 	}
@@ -117,6 +118,7 @@ public class Session {
 	}
 
 	public void run() {
+		if (action == null) return;
 
 		switch (action) {
 			case CREATE_SCHEMA:
@@ -133,7 +135,7 @@ public class Session {
 
 	private void createSchema() {
 		try {
-			final String dbType = switch(databaseType) {
+			final String dbType = switch (databaseType) {
 				case DatabaseType.atps -> "autonomous";
 				case DatabaseType.db19c -> "db19c";
 				case DatabaseType.db21c -> "db21c";
@@ -142,7 +144,7 @@ public class Session {
 
 			final String hostname = InetAddress.getLocalHost().getHostName();
 
-			final String uri = String.format("https://api.atlas-controller.oraclecloud.com/ords/atlas/admin/database?type=%s&hostname=%s",dbType, hostname);
+			final String uri = String.format("https://api.atlas-controller.oraclecloud.com/ords/atlas/admin/database?type=%s&hostname=%s", dbType, hostname);
 
 			final HttpRequest request = HttpRequest.newBuilder()
 					.uri(new URI(uri))
@@ -165,22 +167,22 @@ public class Session {
 					createSchemaWithORDS(response.body());
 				}
 				else {
-					throw new TestException(TestException.CREATE_SCHEMA_REST_ENDPOINT_ISSUE,
-							new IllegalStateException("HTTP/S status code: "+response.statusCode()));
+					throw new TestException(CREATE_SCHEMA_REST_ENDPOINT_ISSUE,
+							new IllegalStateException("HTTP/S status code: " + response.statusCode()));
 				}
 			}
 		}
 		catch (UnknownHostException e) {
-			throw new TestException(TestException.UNKNOWN_HOSTNAME, e);
+			throw new TestException(UNKNOWN_HOSTNAME, e);
 		}
 		catch (URISyntaxException e) {
-			throw new TestException(TestException.WRONG_MAIN_CONTROLLER_URI, e);
+			throw new TestException(WRONG_MAIN_CONTROLLER_URI, e);
 		}
 		catch (IOException e) {
-			throw new TestException(TestException.WRONG_MAIN_CONTROLLER_REST_CALL, e);
+			throw new TestException(WRONG_MAIN_CONTROLLER_REST_CALL, e);
 		}
 		catch (InterruptedException e) {
-			throw new TestException(TestException.WRONG_MAIN_CONTROLLER_REST_CALL, e);
+			throw new TestException(WRONG_MAIN_CONTROLLER_REST_CALL, e);
 		}
 	}
 
@@ -193,8 +195,6 @@ public class Session {
 			Database database = new ObjectMapper().readValue(jsonInformation, Database.class);
 			database = new ObjectMapper().readValue(database.getDatabase(), Database.class);
 
-			System.out.println(database);
-
 			final String uri = String.format("https://%s.oraclecloudapps.com/ords/admin/_/sql", database.getHost());
 
 			final HttpRequest request = HttpRequest.newBuilder()
@@ -205,10 +205,10 @@ public class Session {
 							"Pragma", "no-cache",
 							"Cache-Control", "no-store")
 					.POST(HttpRequest.BodyPublishers.ofString(String.format("""
-							create user hibernate_orm_test_%s identified by "Oracle_19_Password" DEFAULT TABLESPACE DATA TEMPORARY TABLESPACE TEMP;
-							alter user hibernate_orm_test_%s quota unlimited on data;
-							grant CREATE SESSION, RESOURCE, CREATE VIEW, CREATE SYNONYM, CREATE ANY INDEX, EXECUTE ANY TYPE to hibernate_orm_test_%s;
-							""",runID, runID, runID)))
+							create user %s_%s identified by "%s" DEFAULT TABLESPACE DATA TEMPORARY TABLESPACE TEMP;
+							alter user %s_%s quota unlimited on data;
+							grant CREATE SESSION, RESOURCE, CREATE VIEW, CREATE SYNONYM, CREATE ANY INDEX, EXECUTE ANY TYPE to %s_%s;
+							""", user, runID, password, user, runID, user, runID)))
 					.build();
 
 			try (HttpClient client = HttpClient
@@ -221,28 +221,28 @@ public class Session {
 				final HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
 				if (response.statusCode() == 200) {
-					System.out.println(response.body());
+					System.out.println("Done.");
 				}
 				else {
-					throw new TestException(TestException.ATPS_REST_ENDPOINT_ISSUE,
-							new IllegalStateException("HTTP/S status code: "+response.statusCode()));
+					throw new TestException(ATPS_REST_ENDPOINT_ISSUE,
+							new IllegalStateException("HTTP/S status code: " + response.statusCode()));
 				}
 			}
 		}
 		catch (JsonMappingException e) {
-			throw new TestException(TestException.BAD_CREATE_SCHEMA_RESPONSE,e);
+			throw new TestException(BAD_CREATE_SCHEMA_RESPONSE, e);
 		}
 		catch (JsonProcessingException e) {
-			throw new TestException(TestException.BAD_CREATE_SCHEMA_RESPONSE,e);
+			throw new TestException(BAD_CREATE_SCHEMA_RESPONSE, e);
 		}
 		catch (URISyntaxException e) {
-			throw new TestException(TestException.WRONG_ATPS_REST_CALL,e);
+			throw new TestException(WRONG_ATPS_REST_CALL, e);
 		}
 		catch (IOException e) {
-			throw new TestException(TestException.WRONG_ATPS_REST_CALL,e);
+			throw new TestException(WRONG_ATPS_REST_CALL, e);
 		}
 		catch (InterruptedException e) {
-			throw new TestException(TestException.WRONG_ATPS_REST_CALL,e);
+			throw new TestException(WRONG_ATPS_REST_CALL, e);
 		}
 	}
 
