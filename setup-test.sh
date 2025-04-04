db43185b1952aebdc53053ca1b60cbde15c748ce
loic_lefevre <loic.lefevre@oracle.com>
2025-04-04 17:21:03 +0200

    Change

--- setup-test.sh
+++ setup-test.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 set -e
+echo "::group::📦 Downloading test"
+wget https://github.com/loiclefevre/test/releases/download/${VERSION}/test-linux-x86_64.tar.gz
+echo "::endgroup::"
+
 echo "::group::📦 Unpacking test"
 tar -xf test-linux-x86_64.tar.gz
 echo "::endgroup::"
