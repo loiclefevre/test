7e58ecb37f00ba82a8c65f8a6505ec2d6bc59baa
loic_lefevre <loic.lefevre@oracle.com>
2025-04-22 12:38:42 +0200

    Change 0.0.15

--- setup-test.sh
+++ setup-test.sh
@@ -3,7 +3,7 @@
 set -e
 
 echo "::group::📦 Downloading test"
-wget https://github.com/loiclefevre/test/releases/download/${VERSION}/test-linux-x86_64.tar.gz
+curl -o ${GITHUB_ACTION_PATH}/test-linux-x86_64.tar.gz https://github.com/loiclefevre/test/releases/download/${VERSION}/test-linux-x86_64.tar.gz
 echo "::endgroup::"
 
 echo "::group::📦 Unpacking test"
