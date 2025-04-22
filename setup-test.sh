d9d58213644e02850d846b74e5bac7f7234529f6
loic_lefevre <loic.lefevre@oracle.com>
2025-04-22 16:11:07 +0200

    Change 0.0.21

--- setup-test.sh
+++ setup-test.sh
@@ -2,11 +2,7 @@
 
 set -e
 
-echo "VERSION: ${VERSION}"
-echo "GITHUB_ACTION_PATH: ${GITHUB_ACTION_PATH}"
-id
-
-echo "::group::📦 Downloading test"
+echo "::group::🔽 Downloading test"
 wget https://github.com/loiclefevre/test/releases/download/${VERSION}/test-linux-x86_64.tar.gz -O ${GITHUB_ACTION_PATH}/test-linux-x86_64.tar.gz -q
 echo "::endgroup::"
 
