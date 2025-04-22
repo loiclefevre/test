a6d2928eabf3af43b63081e87e4850c7e1add659
loic_lefevre <loic.lefevre@oracle.com>
2025-04-22 18:35:17 +0200

    Change 0.0.26

--- src/main/java/com/oracle/testpilot/Main.java
+++ src/main/java/com/oracle/testpilot/Main.java
@@ -24,7 +24,7 @@ public class Main {
 		System.setProperty("jdk.internal.httpclient.disableHostnameVerification", "true");
 	}
 
-	public static final String VERSION = "0.0.25";
+	public static final String VERSION = "0.0.26";
 
 	public static void main(final String[] args) {
 		final long startTime = System.currentTimeMillis();
--- src/main/java/com/oracle/testpilot/Session.java
+++ src/main/java/com/oracle/testpilot/Session.java
@@ -176,6 +176,7 @@ public class Session {
 
 				default:
 					displayUsage();
+					System.out.println("Wrong arg: "+arg);
 					throw new TestPilotException(UNKNOWN_COMMAND_LINE_ARGUMENT);
 			}
 		}
