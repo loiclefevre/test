0cb4cebcd61539079cb79b4dcdcb20be9e601c64
loic_lefevre <loic.lefevre@oracle.com>
2025-04-28 13:35:47 +0200

    Change 0.0.37

--- src/main/java/com/oracle/testpilot/Main.java
+++ src/main/java/com/oracle/testpilot/Main.java
@@ -24,7 +24,7 @@ public class Main {
 		System.setProperty("jdk.internal.httpclient.disableHostnameVerification", "true");
 	}
 
-	public static final String VERSION = "0.0.36";
+	public static final String VERSION = "0.0.37";
 
 	public static void main(final String[] args) {
 		final long startTime = System.currentTimeMillis();
